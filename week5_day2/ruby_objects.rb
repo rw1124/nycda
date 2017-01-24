a819b36ba92e588ddbdb331c451df237519ea3b2
Ross Weidner <rossweidner@Rosss-MacBook-Pro.local>
2017-01-24 17:55:12 -0600

    Added the calculator

--- week5_day2/ruby_objects.rb
+++ week5_day2/ruby_objects.rb
@@ -1,12 +1,34 @@
 class Calculator
 
-	attr_accessor :add, :subtract, :multiply, :divide
+	def add
+    puts "Which numbers would you like to add?"
+    n1 = gets.to_i
+    n2 = gets.to_i
+    answer = n1 + n2
+    puts "The sum is...#{answer}"
+  end
 
-	def value1
+ def subtract
+    puts "Which numbers would you like to subtract?"
+    n1 = gets.to_i
+    n2 = gets.to_i
+    answer = n1 - n2
+    puts "The sum is...#{answer}"
+  end
 
-	end
+ def multiply
+    puts "Which numbers would you like to multiply?"
+    n1 = gets.to_i
+    n2 = gets.to_i
+    answer = n1 * n2
+    puts "The sum is...#{answer}"
+  end
 
-	def value2
-
-	end 
-end 
+ def divide
+    puts "Which numbers would you like to divide?"
+    n1 = gets.to_i
+    n2 = gets.to_i
+    answer = n1 / n2
+    puts "The sum is...#{answer}"
+  end
+end
